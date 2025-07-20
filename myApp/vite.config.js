994d4e59e5ec0aee555fcb26e63668b8bddb9328
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-20 19:26:24 +0100

    '.'

--- myApp/vite.config.js
+++ myApp/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./Build_files",
+  base: "/MealBowlApp/Build_files",
   plugins: [react()],
 });
