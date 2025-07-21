2a63052f7f021a92b10510966b461a510c394cc6
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-21 18:50:14 +0100

    '.'

--- myApp/src/HomePage.jsx
+++ myApp/src/HomePage.jsx
@@ -1,12 +1,24 @@
 import BowlImage from "./BowlImage.jsx";
 import "./BowlImage.css";
-import "./HomePage.css"
+import "./HomePage.css";
 function RenderBowls() {
   return (
     <>
       <h1 className="center">Tasty and healthy food bowls</h1>
-      <h3 className="center caption">Fuel your body with delicious healthy food delivered right to you</h3>
-      <a href="#Bowls" className="center link">Explore bowls</a>
+      <h3 className="center caption">
+        Fuel your body with delicious healthy food delivered right to you
+      </h3>
+      <a href="#Bowls" className="center link">
+        Explore bowls
+      </a>
+      <div className="push">
+        <h2>Contact us: </h2>
+        <div className="pushChildAlign">
+          <p>Owner: Jyoti Sharma</p>
+          <p>Email: gobbledygook@gmail.com</p>
+          <p>Phone number: 05406405640606</p>
+        </div>
+      </div>
       <div id="Bowls" className="flex">
         <BowlImage css="reSize" />
         <BowlImage css="reSize red" />
