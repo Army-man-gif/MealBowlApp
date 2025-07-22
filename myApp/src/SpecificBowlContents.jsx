b161bb7e7a5b25378f2efa3a62c6447999b5a4be
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-22 01:03:16 +0100

    '.'

--- myApp/src/SpecificBowlContents.jsx
+++ myApp/src/SpecificBowlContents.jsx
@@ -14,10 +14,52 @@ function Contents() {
     ],
     "Soya-Chunk-High-Protein-Bowl": [
       "100g cooked soya chunks (masala sautéed)",
-      "1/2 cup mashed sweet potato",
+      "1/2 cup mashed sweet potato or mashed potato",
       "1/4 cup steamed broccoli and beans",
       "2 tbsp roasted peanuts",
       "Sprinkle of chat masala and lemon juice",
+    ],
+    "Rajma Superfood Bowl": [
+      "3/4 cup boiled rajma (kidney beans)",
+      "1/2 cup cooked red rice or millets",
+      "1/2 cup mixed vegetables (carrot, peas, beans)",
+      "1 tsp mustard oil or ghee",
+      "Toppings: Fresh coriander, cumin powder, lemon juice",
+    ],
+    "Tofu-Stir-Fry-Bowl": [
+      "150g tofu (pan-grilled with turmeric, garlic, and pepper)",
+      "1/2 cup cooked oats or barley",
+      "1/4 cup capsicum, mushroom, and baby corn stir-fry",
+      "1 tsp sesame oil",
+      "Toppings: Toasted sesame seeds and soy sauce drizzle",
+    ],
+    "Eggless-Bhurji-&-Oats-Bowl": [
+      "1/2 cup moong dal chilla crumble (eggless bhurji style)",
+      "1/2 cup cooked masala oats",
+      "1/4 cup steamed peas and cauliflower",
+      "1 tsp ghee or coconut oil",
+      "Toppings: Mint, lemon, roasted cumin powder",
+    ],
+    "Egg-Bhurji-Nutrition-Bowl": [
+      "3 egg whites + 2 whole eggs (bhurji with onion, tomato, green chili)",
+      "1/2 cup cooked oats or rolled oats khichdi",
+      "1/4 cup steamed cauliflower or peas",
+      "1 tsp ghee or butter",
+      "Toppings: Mint, lemon, and flaxseed powder",
+    ],
+    "Fish-&-Veggie-Grain-Bowl": [
+      "150g grilled fish (pomfret or salmon, spiced with turmeric, garlic)",
+      "1/2 cup cooked red rice or foxtail millet",
+      "1/2 avocado or 1 tsp flaxseed oil",
+      "1/2 cup sautéed kale/spinach + carrots",
+      "Toppings: Roasted sesame seeds, green chutney drizzle",
+    ],
+    "Chicken-Tikka-Macro-Bowl": [
+      "150g grilled chicken tikka (marinated in curd + spices)",
+      "1/2 cup cooked millets or jeera brown rice",
+      "1/4 cup cucumber-tomato-onion salad",
+      "1 tbsp hung curd mint dip",
+      "1 tsp ghee for flavor",
     ],
   };
   const bowlInfo = information[bowlID]
