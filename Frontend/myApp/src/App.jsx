eefb254779a82cd12967b77a029979ec424b59ff
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-31 10:49:30 +0100

    '.'

--- Frontend/myApp/src/App.jsx
+++ Frontend/myApp/src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/MealBowlApp/docs">
+      <BrowserRouter basename="/MealBowlApp/Frontend/docs">
         <Routes>
           <Route path="/" element={<RenderBowls />} />
           <Route path="/contents/:bowlID" element={<Contents />} />
