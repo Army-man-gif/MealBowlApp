1f7417c86bf84420048022f3ed5fb9e1212f3fea
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-07-31 11:57:21 +0100

    '.'

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+console.log("Vite configuration loaded");
 // https://vite.dev/config/
 export default defineConfig({
   base: "/MealBowlApp/docs/",
