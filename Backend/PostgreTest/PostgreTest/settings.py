2eb78b19bb90f6273af5d280f872aa0e0db228b7
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-01 18:18:08 +0100

    '.'

--- Backend/PostgreTest/PostgreTest/settings.py
+++ Backend/PostgreTest/PostgreTest/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = "PostgreTest.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    "sqliteConfig": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myDB',
+        'USER': 'newUser',
+        'PASSWORD': 'testPass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
