98fa7e9164ba6953e20095a8bae100a496e85042
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-02 19:12:39 +0100

    sfd

--- Backend/PostgreTest/testDB/views.py
+++ Backend/PostgreTest/testDB/views.py
@@ -2,31 +2,54 @@ from django.shortcuts import render,redirect
 from .models import Data
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,login
 
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 
 def createUser(request,username,password,email):
-    user = User.objects.create_user(username=username, email=email)
-    user.set_password(password)
+    content_type = ContentType.objects.get_for_model(User)
+    permission = Permission.objects.create(
+        codename = "admin",
+        name = "Admin access granted",
+        content_type=content_type,
+    )
+    user = User.objects.create_user(username=username,password=password email=email)
+    user.user_permissions.add(permission)
     user.save()
-
+# user._perm_cache  = None (permission cache clearing)
 def validateUser(request,username,password,email):
     try:
-        user = authenticate(request,username,password)
-        if(user & user.email==email):
+        user = authenticate(request,username=username,password=password)
+        if(user is not None and user.email==email):
             return True
     except User.DoesNotExist:
         pass
     return False
-    
+def getUser(request,username,password,email):
+    if(validateUser(request,username,password,email)):
+        user = User.objects.get(username=username,email=email)
+        return user
+    return None
 def deleteUser(request,username,password,email):
     if(validateUser(request,username,password,email)):
         user = User.objects.get(username=username,email=email)
         user.delete()
+        return HttpResponse("User deleted")
+    return HttpResponse("Invalid credentials")
         
-        
-        
-        
+# ---------------------------------------------------------------------------------------------------------------   
+
+def login(request,username,password,email):
+    user = authenticate(request, username=username, password=password)
+    if(user is not None and user.email==email):
+        login(request, user)
+
+def logout(request):
+    logout(request)
+
+# ---------------------------------------------------------------------------------------------------------------   
+
 def DatabaseLandingPage(request):
     return HttpResponse("Welcome to database testing")
 def landingPage(request):
