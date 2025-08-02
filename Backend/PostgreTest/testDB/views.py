dc54193a29b89acc7a39ea34d39c9888d8c74915
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-02 18:26:25 +0100

    sdf

--- Backend/PostgreTest/testDB/views.py
+++ Backend/PostgreTest/testDB/views.py
@@ -2,11 +2,27 @@ from django.shortcuts import render,redirect
 from .models import Data
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
-def UserLogic(request,username,password,email):
+
+def createUser(request,username,password,email):
     user = User.objects.create_user(username=username, email=email)
     user.set_password(password)
     user.save()
+
+def validateUser(request,username,password,email):
+    try:
+        user = authenticate(request,username,password)
+        if(user & user.email==email):
+            return True
+    except User.DoesNotExist:
+        pass
+    return False
+    
+def deleteUser(request,username,password,email):
+    if(validateUser(request,username,password,email)):
+        user = User.objects.get(username=username,email=email)
+        user.delete()
 def DatabaseLandingPage(request):
     return HttpResponse("Welcome to database testing")
 def landingPage(request):
@@ -20,5 +36,8 @@ def addData(request,value):
     return redirect("getData",value=value)
     
 def displayData(request,value):
-    val  = Data.objects.get(name=value)
-    return HttpResponse(val.name)
+    try:
+        val  = Data.objects.get(name=value)
+        return HttpResponse(val.name)
+    except Data.DoesNotExist:
+        return redirect("addData",value=value)
