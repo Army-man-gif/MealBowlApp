1dab59b7d5a91b87fd2fcbf816e89c30b1ba5c1e
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-08 12:59:52 +0100

    '.'

--- Backend/PostgreTest/testDB/urls.py
+++ Backend/PostgreTest/testDB/urls.py
@@ -3,8 +3,6 @@ from . import views
 urlpatterns = [
     path("", views.DatabaseLandingPage,name="intro"),
     path("createUser/", views.createUser,name="createUser"),
-    path("getData/<str:value>/", views.displayData,name="getData"),
-    path("addData/<str:value>/", views.addData, name="addData"),
     path("setToken/",views.setToken,name="setToken"),
     path("getToken/",views.get_csrf_token,name="getToken"),
     path("deleteUser/<str:username>/", views.deleteUser,name="deleteUser"),
--- Backend/PostgreTest/testDB/views.py
+++ Backend/PostgreTest/testDB/views.py
@@ -149,16 +149,4 @@ def DatabaseLandingPage(request):
 def landingPage(request):
     return HttpResponse("Welcome to home")
 # Create your views here.
-def addData(request,value):
-    try:
-        Data.objects.get(name=value)
-    except Data.DoesNotExist:
-        Data.objects.create(name=value)
-    return redirect("getData",value=value)
-    
-def displayData(request,value):
-    try:
-        val  = Data.objects.get(name=value)
-        return HttpResponse(val.name)
-    except Data.DoesNotExist:
-        return redirect("addData",value=value)
+
