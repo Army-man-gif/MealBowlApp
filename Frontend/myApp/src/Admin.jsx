614f55beeb54121a796d3fb0176e6bef894721db
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-14 17:16:47 +0100

    '.'

--- Frontend/myApp/src/Admin.jsx
+++ Frontend/myApp/src/Admin.jsx
@@ -1,20 +1,27 @@
+import { useEffect } from "react";
 import { setCookie, getCookieFromBrowser } from "./auth.js";
 
-async function AdminPage() {
-  const getAll = await fetch(
-    "https://mealbowlapp.onrender.com/databaseTesting/getEverything/",
-    {
-      credentials: "include",
-    },
-  );
-  const contentType = getAll.headers.get("content-type");
-  let result;
-  if (contentType && contentType.includes("application/json")) {
-    result = await getAll.json();
-  } else {
-    result = await getAll.text();
-  }
-  console.log(result);
+function AdminPage() {
+  useEffect(() => {
+    const fetchAll = async () => {
+      const getAll = await fetch(
+        "https://mealbowlapp.onrender.com/databaseTesting/getEverything/",
+        {
+          credentials: "include",
+        },
+      );
+      const contentType = getAll.headers.get("content-type");
+      let result;
+      if (contentType && contentType.includes("application/json")) {
+        result = await getAll.json();
+      } else {
+        result = await getAll.text();
+      }
+      console.log(result);
+    };
+    fetchAll();
+  }, []);
+
   return (
     <>
       <div>Hi</div>
