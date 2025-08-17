7371fe9723bccbd6988bfc6dc451b4c4f27e20e4
Armaan Khaitan <khaitanarmaan@gmail.com>
2025-08-18 00:24:23 +0100

    '.'

--- Frontend/myApp/src/MainCheckout.jsx
+++ Frontend/myApp/src/MainCheckout.jsx
@@ -44,54 +44,56 @@ function MainCheckout() {
     }
     let renderingData = [];
     setRows(max * 3 + 2);
+    const keys = Object.keys(CheckoutData);
     const usernameKey = Object.keys(CheckoutData)[0];
     const userData = CheckoutData[usernameKey];
-    const priceKey = Object.keys(CheckoutData)[1];
-    const priceData = CheckoutData[priceKey];
+    const priceData = userData["TotalPrice"];
     console.log(
+      "keys: ",
+      keys,
       "usernameKey: ",
       usernameKey,
       "userData: ",
       userData,
-      "priceKey: ",
-      priceKey,
       "priceData",
       priceData,
     );
 
     renderingData.push(<div key={`User-${usernameKey}-0`}>{usernameKey}</div>);
     Object.entries(userData).forEach(([key2, value2], j) => {
-      renderingData.push(
-        <div style={{ gridColumn: "1" }} key={`BowlName-${key2}-${j}`}>
-          {key2}
-        </div>,
-      );
-      renderingData.push(
-        <div style={{ gridColumn: "2" }} key={`NoOfBowls-${key2}-${j}`}>
-          Number of bowls: {value2["NumberofBowls"]}
-        </div>,
-      );
-      renderingData.push(
-        <div style={{ gridColumn: "3" }} key={`Price-${key2}-${j}`}>
-          Price of this part of the order: {value2["Price"]}
-        </div>,
-      );
-      renderingData.push(
-        <input
-          disabled={checkingOut}
-          key={`Alter-Order-${key2}-${j}`}
-          type="number"
-          value={value2["NumberofBowls"]}
-          placeholder="Change your order quantity"
-          style={{ gridColumn: "1 / -1" }}
-        ></input>,
-      );
-      renderingData.push(
-        <div
-          key={`Space-${key2}-${j}`}
-          style={{ gridColumn: "1 / -1", height: "20px" }}
-        ></div>,
-      );
+      if (j < Object.keys(userData).length - 1) {
+        renderingData.push(
+          <div style={{ gridColumn: "1" }} key={`BowlName-${key2}-${j}`}>
+            {key2}
+          </div>,
+        );
+        renderingData.push(
+          <div style={{ gridColumn: "2" }} key={`NoOfBowls-${key2}-${j}`}>
+            Number of bowls: {value2["NumberofBowls"]}
+          </div>,
+        );
+        renderingData.push(
+          <div style={{ gridColumn: "3" }} key={`Price-${key2}-${j}`}>
+            Price of this part of the order: {value2["Price"]}
+          </div>,
+        );
+        renderingData.push(
+          <input
+            disabled={checkingOut}
+            key={`Alter-Order-${key2}-${j}`}
+            type="number"
+            value={value2["NumberofBowls"]}
+            placeholder="Change your order quantity"
+            style={{ gridColumn: "1 / -1" }}
+          ></input>,
+        );
+        renderingData.push(
+          <div
+            key={`Space-${key2}-${j}`}
+            style={{ gridColumn: "1 / -1", height: "20px" }}
+          ></div>,
+        );
+      }
     });
     console.log(priceData);
     renderingData.push(
